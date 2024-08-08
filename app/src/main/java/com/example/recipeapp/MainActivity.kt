2618e06a0d10d1f5588f5666b92ae9d67e35aaa8
Mohamed khedr <khedrkhedr370@gmail.com>
2024-08-08 21:22:53 +0300

    update

--- app/src/main/java/com/example/recipeapp/MainActivity.kt
+++ app/src/main/java/com/example/recipeapp/MainActivity.kt
@@ -11,5 +11,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        
     }
 }
